42aef165837a8adf1cafd7d390278dd5c5293991
MaximTkachenko <maksim_tk@icloud.com>
2024-11-03 18:22:57 +0300

    initial commit

---
+++ seminar1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+
+#include "my_add.hpp"
+
+int main(int argc, char *argv[]){
+    //check how many arguments were given
+    if (argc != 3){
+        std::cerr << "Provide exactly 2 arguments\n";
+        return 1;
+    }
+
+    int a = std::atoi(argv[1]);
+    int b = std::atoi(argv[2]);
+    std::cout<<my_add(a, b)<<'\n';
+    return 0;
+}
